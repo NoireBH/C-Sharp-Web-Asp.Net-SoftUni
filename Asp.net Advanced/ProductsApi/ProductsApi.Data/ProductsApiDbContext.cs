4ed054dc46eaba2f0d04430af8b9e94ab3837ac6
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2023-12-17 16:15:45 +0200

    added initial design of the api and dbcontext with a products class

---
+++ Asp.net Advanced/ProductsApi/ProductsApi.Data/ProductsApiDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsApi.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsApi.Data
+{
+	public class ProductsApiDbContext : DbContext
+	{
+		public ProductsApiDbContext(DbContextOptions<ProductsApiDbContext> options)
+			: base(options)
+		{
+			this.Database.EnsureCreated();
+		}
+
+		public DbSet<Product> Products { get; set; } = null!;
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{			
+			base.OnModelCreating(builder);
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+		}
+	}
+}
